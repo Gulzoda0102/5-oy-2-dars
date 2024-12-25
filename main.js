13326145a8e5bfab79d7625600924d0bcc379375
Gulzoda0102 <zodait2024@gmail.com>
2024-12-25 10:07:57 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,160 @@
+// console.log("Gulzoda" + "\n" +   "Maftuna");
+// console.log("Sitora");
+// console.log("G + G");
+// console.log(17+3);
+// console.log(17 + 3 + "3");
+// console.log("17" + 3 + 3);
+
+
+// var
+// let
+// const
+
+// var username = "Gulzoda";
+// var username = "Maftuna";
+
+// console.log(username);
+
+// let ism = "Gulzoda";
+// // let ism = "Lily";
+// console.log(ism);
+
+
+// let number = 18;
+// let number2 = 2;
+// console.log("Javob:", number + number2);
+// prompt("number + number ")
+
+
+
+
+// // prompt("Enter your name");
+
+
+
+// console.log(username, surname, age);
+
+
+// prompt("Enter your name")
+
+// let name = prompt("Enter your name");
+
+
+// console.log(username, surname, age);
+// let username = prompt("Enter your name");
+
+// let surname = prompt("Enter your surname");
+
+
+let name = prompt("Ismingizni kiriting:");
+let surname = prompt("Familiyangizni kiriting:");
+let age = prompt("Yoshingizni kiriting:");
+let subject = prompt("Yoqtirgan faningizni yozing:");
+let district = prompt("Yashaydigan tumaningizni kiriting:");
+
+
+console.log("Ism: " + name);
+console.log("Familiya: " + surname);
+console.log("Yosh: " + age); 
+console.log("Yoqtirgan fan: " + subject);
+console.log("Yashash manzili: " + district);
+
+
+
+
+// const num = + prompt("Raqam kiriting");
+// const num2 = + prompt("Ikkinchi raqamni kiriting");
+
+// console.log(num + num2);
+
+
+
+
+
+// console.log(`Javob:${num}`);
+
+
+// console.log(`${num} + ${num2} = ${num + num2}`);
+
+// const num = parseInt(prompt("Birinchi sonni kiriting:"));
+// const num2 = parseInt(prompt("Ikkinchi sonni kiriting:"));
+
+// console.log(`${num} + ${num2} = ${num + num2}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dollor maydalidigan narsa qib kelish kere dollor narhi ni chiqazib berish kere summasini 
+
+
+
+
+
+
+
+
+
+
+
+// const username = prompt("Are you married");
+// alert(username)
+// confirm("Are you married?");
+// ishlatish kere;
+
+
+
+
+// const dollar = parseFloat(prompt("Iltimos, dollar summasini kiriting:"));
+
+// // Agar kiritilgan qiymat raqam bo'lsa, summani chiqaring
+// if (!isNaN(dollar) && dollar > 0) {
+//     console.log(`Siz kiritgan dollar summasi: $${dollar.toFixed(2)}`);
+// } else {
+//     console.log("Iltimos, to'g'ri raqam kiriting!");
+// }
+
+
+// const ism ="Guli";
+// const ism1 = "Mapu";
+// const raqam = 888;
+// console.log(ism, raqam);
+
+
+
+
+
+// // const dollarInput = prompt("Necha dollar borligini kiriting:");
+// // const dollar = parseFloat(dollarInput.replace('$', '').trim());
+// // const exchangeRate = 12800; // 1 USD = 12,000 UZS (misol uchun)
+
+// // if (!isNaN(dollar) && dollar > 0) {
+// //     const sumInUzs = dollar * exchangeRate;
+// //     console.log(`${dollar} dollar = ${sumInUzs.toLocaleString()} so'm`);
+// // } else {
+// //     console.log("Iltimos, to'g'ri raqam kiriting!");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
